28b30f918645d28b1a21cc056a2130eace53caf7
Karina Myronova <karinamyronova@MacBookAir.fritz.box>
2024-12-13 15:31:32 +0100

    hw22

--- js/task-3.js
+++ js/task-3.js
@@ -18,17 +18,13 @@ console.log(getElementWidth("200px", "0px", "0px")); // 200
 
 
 function checkForSpam(message) {
-   
     const lowerCaseMessage = message.toLowerCase();
-    
     return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
-  }
-  
+}
 
-  console.log(checkForSpam("Latest technology news")); // false
-  console.log(checkForSpam("JavaScript weekly newsletter")); // false
-  console.log(checkForSpam("Get best sale offers now!")); // true
-  console.log(checkForSpam("Amazing SaLe, only tonight!")); // true
-  console.log(checkForSpam("Trust me, this is not a spam message")); // true
-  console.log(checkForSpam("Get rid of sPaM emails. Our book is on sale!")); // true
-  console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Amazing Sale, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of SPAM emails. Our book is on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
